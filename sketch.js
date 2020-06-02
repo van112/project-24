92d01a9535b239418de0581b0ab20991e1ca25b5
van112 <64940663+van112@users.noreply.github.com>
2020-06-01 20:16:17 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,37 +11,21 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	paperBody= ellipse(56,46,55,55); 
 
-	paperBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
-	paperBody.shapeColor= color(255,0,0);
-	World.add(world, paperBody);
-	boxPosition=width/2-100 
-	boxY=610;
-	
+
 	
 
 	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-	 World.add(world, ground);
-	 boxleftSprite=createSprite(boxPosition, boxY, 20,100); 
-	 boxleftSprite.shapeColor=color(255,0,0);
-	 boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} ); 
-	 World.add(world, boxLeftBody);
-	 boxRightSprite=createSprite(boxPosition+200, boxY, 20,100); 
-	 boxRightSprite.shapeColor=color(255,0,0);
-	 boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} ); 
-	 World.add(world, boxRightBody);
-	 boxBase=createSprite(boxPosition+100, boxY+40, 200,20); 
-	 boxBase.shapeColor=color(255,0,0); 
-	 boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
-	 World.add(world, boxBottomBody);
+	ground = new Ground(600,height,1600,20);
+	paper=new Paper(200,450,40); 
+	dustbin=new Dustbin(1200,650);
+
 	 
 
 
@@ -53,9 +37,17 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  
-  drawSprites();
+  ground.display();
+  paper.display();
+  dustbin.display();
+
  
+}
+function keyPressed(){
+	if (keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+
+	}
 }
 
 
